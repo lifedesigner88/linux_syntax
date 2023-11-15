90726e2297d7e21f82061629855e74285fa58355
lifedesigner88@gmail.com <lifedesigner88@gmail.com>
2023-11-15 10:29:44 +0900

    if_statement

---
+++ 3.if_statements_practice.sh
@@ -0,0 +1,46 @@
+
+
+
+if [ 조건 ]; then
+    echo "file exists"
+
+elif [ 다른조건 ]; then
+
+else
+    echo "hello java"
+fi
+
+for [변수] in [항목들]; do
+done
+
+
+
+
+
+-f // 파일 체크
+-d // 디렉토리 체크
+
+
+
+chmod  // 
+
+
+if 문을 사용하여 만약 현재 폴더에 first.txt 라는 파일이 있으면 exist 출력
+
+# -f ./if statement 실습 코드
+file_name="first.txt"
+if [ -f "$file_name" ]; then
+    echo "$file_name exists"
+
+else
+    echo "$file_name dose not exit"
+fi
+# *****************************
+
+
+
+
+
+for [변수] in [항목들]; do
+done
+
