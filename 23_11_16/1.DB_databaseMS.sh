a70e7aa7de7e25d5343cf04fa6d5d099afb7fdf4
lifedesigner88@gmail.com <lifedesigner88@gmail.com>
2023-11-16 09:55:06 +0900

    23.11.16 1교시

---
+++ 23_11_16/1.DB_databaseMS.sh
@@ -0,0 +1,15 @@
+DBMS Data Base Management System
+
+Mysql, MariaDB, Oracle, Postgress       //  관계형 (RDB)    Mysql, MariaDB 
+MonggoDB, Redits                        //  비관계형 No Sql, 
+
+
+Spring boot 
+
+
+
+
+Mysql => 상업용으로 바뀌었음. 
+Mysql => MariaDB (mysql 업그레이드 버전);
+
+
