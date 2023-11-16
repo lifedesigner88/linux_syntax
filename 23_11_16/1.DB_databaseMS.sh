e481f2d9bac1b3a4923821d8f979fc46f81a0c46
lifedesigner88@gmail.com <lifedesigner88@gmail.com>
2023-11-16 12:19:06 +0900

    end

--- 23_11_16/1.DB_databaseMS.sh
+++ 23_11_16/1.DB_databaseMS.sh
@@ -8,8 +8,45 @@ Spring boot
 
 
 
+Mysql               //      상업용으로 바뀌었음. 
+Mysql               //      MariaDB (mysql 업그레이드 버전);
 
-Mysql => 상업용으로 바뀌었음. 
-Mysql => MariaDB (mysql 업그레이드 버전);
+스키마 = 데이터베이스 
 
+
+스키마 하나에 테이블 하나 
+컬럼 => 열 => 속성 (atribute)
+
+
+
+데이터를 읽고 하는 작업들이 엑셀보다는 DB가 훨씬 잘되어있다. (속도가 빠르다. 웹서비스)
+
+데이터의 정합성(정확도가)떨어진다. 
+
+
+
+RDB는 개발 난이도가 올라간다. (장바구니) (몽고 DB 설계)
+
+
+DMBS 에서 사용하는 프로그래밍 Language      //  SQL
+CRUD
+    Create, Read, Update, Delete
+
+
+
+
+SQL 3가지 구분
+
+    DDL 
+        Data Definition Language
+        데이터베이스나 테이블 등을 생성, 삭제하거나 그 구조를 변경
+        CREATE, ALTER, DROP    (테이블 통체로 날리기 DROP)
+    DML
+        Data Manipulation Language
+        데이터 베이스에 저장된 데이터를 처리하거나 조회
+        INSERT, UPDATE DELETE, SELECT 등 ()
+    DCL
+        Data Control Language  (마케팅 부서 SELECT 계정)
+        사용자 권한 부여 등
+        GRANT, REVOKE 등 (부여, 회수)
 
---
+++ 23_11_16/2.DB_advanced.sh
@@ -0,0 +1,17 @@
+1. DB Dump
+2. 기술 개발        //      협업.
+
+
+
+PK   유일한 식별자  (private key) Null 불가
+FK   외래키
+
+# 환경변수 입력
+고급시스템 설정.
+C:\Program Files\MariaDB 11.3\bin
+
+mariadb -u root -p
+show databases;
+discribe author;
+
+CREATE
